be412abe1807cad7fef2859db01168e48e4fa074
Nilesh Kumar Mishra <nkmishra0102@gmail.com>
2021-06-25 14:49:40 +0530

    First Commit

---
+++ sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum
+{
+  public static void main(String args[]) 
+  {
+    Scanner in=new Scanner(System.in);
+    System.out.println("Enter the number");
+    int n=in.nextInt();
+    System.out.println(n);
+    System.out.println("hello nilesh");
+    in.close();
+  }
+}
